174555adda3ea3d26d7775ec1a12f386acd7748f
zhangrui <crazy25260@hotmail.com>
2020-10-16 12:21:46 +0800

    改版本。

--- src/plugins/vue-video-player.js
+++
@@ -1,9 +0,0 @@
-import VideoPlayer from "vue-video-player";
-require("video.js/dist/video-js.css");
-require("vue-video-player/src/custom-theme.css");
-
-export default {
-  install(Vue) {
-    Vue.use(VideoPlayer);
-  }
-};
